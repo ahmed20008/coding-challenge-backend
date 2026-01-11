60c8cd252a07c185e841190d8c2bc92fac5fe27f
Ahmed <sss.ahm7@gmail.com>
2026-01-11 17:24:39 +0100

    Initialize Docker and set up app with Docker containers

--- api/__init__.py
+++ api/__init__.py
@@ -1,10 +1,9 @@
+from api.applications import create, detail
 from fastapi import APIRouter
-from api import create, list, detail
+from api.applications import list
 
-# Create main router
 router = APIRouter(prefix="/applications", tags=["applications"])
 
-# Include all sub-routers
 router.include_router(create.router)
 router.include_router(list.router)
 router.include_router(detail.router)
